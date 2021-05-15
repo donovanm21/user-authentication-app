06998b15a40f24d5e9688fc583e2da47280443fe
Donovan <donovan@MAC.local>
2021-05-15 12:50:06 +0200

    Update heading

--- member_edit.php
+++ member_edit.php
@@ -73,7 +73,7 @@ require('header.php');
         <div class="row">
           <main class="ms-sm-auto px-md-4">
             <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom ">
-              <h1 class="h2">Update CMS User</h1>
+              <h1 class="h2">Update Member</h1>
               <!--
               <div class="btn-toolbar mb-2 mb-md-0">
                 <div class="btn-group me-2">
