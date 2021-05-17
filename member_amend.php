873b7d11b9e1aceac21889c379afa6f3a6e97803
Donovan <donovan@MAC.local>
2021-05-17 14:57:41 +0200

    Fix user amend redirect

--- member_amend.php
+++ member_amend.php
@@ -8,12 +8,12 @@ if(isset($_GET['id'])) {
     if(isset($_GET['promote'])) {
         $sql = 'UPDATE users SET user_type = "admin" WHERE id = "'.$_GET['id'].'"';
         query($sql);
-        header("location: users.php");
+        header("location: members.php");
     }
     if(isset($_GET['demote'])) {
         $sql = 'UPDATE users SET user_type = "user" WHERE id = "'.$_GET['id'].'"';
         query($sql);
-        header("location: users.php");
+        header("location: members.php");
     }
 }
 
